d25c8e385a6ee9520bebe3ed6cc59ba474d80af5
mahesh-muttinti <maheshmuttinti@gmail.com>
2021-01-30 11:27:29 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import HomePage from './Components/HomePage';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        HomePage / >
+        <
+        /div>
+    );
+}
+
+export default App;
